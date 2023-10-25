0bc7d962e979792d6ff5b940f70291327970a3d9
esNIMA <s.nima.mahmoodian@gmail.com>
2023-10-25 20:30:33 +0330

    Second Commit

---
+++ SequencingSolver/README.md
@@ -0,0 +1,7 @@
+# Sequencing Solver
+
+This is a package for solving sequencing problems. All you have to do is to pass the appropriate form of data structure to the corresponding solver. The solver will run the algorithm and return the optimal sequence and the Gantt chart.
+
+The whole point of this package is to enable students and instructors to easily access the solution and code of the solvers' algorithm.
+
+there will be more algorithms featuring this package.
---
+++ SequencingSolver/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SequencingSolver",
+    version="1.0.0",
+    author="Nima Mahmoodian",
+    author_email="s.nima.mahmoodian@gmail.com",
+    description="Package for solving various sequencing problems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=None,
+    project_urls={
+        "Bug Tracker": "https://github.com/esNIMA/SequencingSolver/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6"
+)
